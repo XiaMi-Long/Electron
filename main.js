678067e010c855d09287a3129e96333e53646d0b
weiwenyu <2454150875@qq.com>
2023-06-27 16:21:40 +0800

    feat: 提交前两章学习

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const { BrowserWindow } = require('electron')
+const { BrowserWindow, app } = require('electron')
+const path = require('path')
+
+app.whenReady().then(() => {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800
+    })
+
+    // mainWindow.loadURL('https://www.electronjs.org/zh/docs/latest/tutorial/quick-start')
+    mainWindow.loadFile(path.resolve(__dirname, 'index.html'))
+})
